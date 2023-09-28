af0a2d44635f1cb60910dd96cc265f2e7f2a7cfd
pedrocasi <pedrocs@exo.pt>
2023-09-28 21:59:41 +0100

    [16.0][MIG] partner_statement_sendf_by_mail

--- partner_statement_send_by_mail/wizards/outstanding_statement_wizard.py
+++ partner_statement_send_by_mail/wizards/outstanding_statement_wizard.py
@@ -11,14 +11,14 @@ class OutstandingStatementWizard(models.TransientModel):
         template = self.env.ref(
             "partner_statement_send_by_mail."
             "email_template_partner_outstanding_statement",
-            False,
+            raise_if_not_found=False,
         )
         compose_form = self.env.ref("mail.email_compose_message_wizard_form", False)
         ctx = dict(
             default_model="res.partner",
             default_res_id=self.env.context.get("active_id", False),
             default_use_template=bool(template),
-            default_template_id=template.id,
+            default_template_id=template and template.id or False,
             default_composition_mode="comment",
             custom_layout="mail.mail_notification_light",
             mark_coupon_as_sent=True,
